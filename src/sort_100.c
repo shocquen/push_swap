8427bcc28100b97644cde804221701f2ad0b6074
Souria-saky HOCQUENGHEM <shocquen@student.42.fr>
2022-01-17 18:28:29 +0100

    start sort 100

--- src/sort_100.c
+++ src/sort_100.c
@@ -6,20 +6,42 @@
 /*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 10:29:58 by shocquen          #+#    #+#             */
-/*   Updated: 2022/01/14 12:47:17 by shocquen         ###   ########.fr       */
+/*   Updated: 2022/01/17 18:28:09 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/* Return the adress of lightest node */
-/* static t_list	**get_lightest(t_list *a, int min, int max)
-{
-	t_list	**ret;
-	int				i;
+ 
 
-	
-} */
+/* Return the index of lightest nb in a chunk*/
+
+static int	get_lightest(t_list *lst, int min, int max)
+{
+	int	i;
+	int	ret;
+	int	size;
+	int	weight;
+
+	i = 0;
+	ret = 0;
+	size = ft_lstsize(lst);
+	while (lst && !lst->next)
+	{
+		if (lst->content >= min && lst->content < max)
+		{
+			if (i > size / 2)
+				weight = size - i;
+			else
+				weight = i;
+			if (ret > weight)
+				ret = weight;
+		}
+		lst = lst->next;
+		i++;
+	}
+	return (weight);
+}
 
 int	ps_sort_100(t_list **a, t_list **b)
 {
